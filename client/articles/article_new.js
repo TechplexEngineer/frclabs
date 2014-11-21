20d54601d8cf12e8f3a047232d494b848078d515
Blake Bourque <Techplex.Engineer@gmail.com>
2014-11-20 22:51:07 -0500

    Add article creation UI, Require login, Add fontawesome

---
+++ client/articles/article_new.js
@@ -0,0 +1,5 @@
+Template.article_new.events({
+	"click .section_help": function (event, template) {
+		alert("Use sections for individual steps of your article.");
+	}
+})
--- client/articles/routes_article.js
+++ client/articles/routes_article.js
@@ -1,1 +1,13 @@
+Router.route('/articles', {name: 'articles'})
 
+Router.route('/article/new', {
+	name: 'New Article',
+	template: 'article_new',
+	onBeforeAction: function () {
+		requireLogin.call(this,'You must be logged in to create a new article.')
+	}
+})
+//order matters here!
+Router.route('/article/:_id', function article () {
+	this.render('article', {data: {id: this.params._id}})
+})
