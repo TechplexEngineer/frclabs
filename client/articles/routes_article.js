3cd99cbfce2840d3c86158449e88246719136cc3
Blake Bourque <Techplex.Engineer@gmail.com>
2014-11-20 21:40:54 -0500

    Trouble with requiring login

--- client/articles/routes_article.js
+++ client/articles/routes_article.js
@@ -1,7 +1,1 @@
-Router.route('/articles', {name: 'articles'})
 
-Router.route('/article/:_id', function article () {
-	this.render('article', {data: {id: this.params._id}})
-})
-
-Router.route('/article/new', {name: 'article_new'})
